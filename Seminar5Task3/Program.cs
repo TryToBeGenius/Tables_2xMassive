2712d58af04c75a5acf8be6e7ab5dd590406f511
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-26 18:06:38 +0500

    Задача 3 решена

---
+++ Seminar5Task3/Program.cs
@@ -0,0 +1,67 @@
+﻿int[,] CreateMatrixRndInt(int row, int column, int min, int max)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) первое измерение массива это Строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // matrix.GetLength(1) второе измерение массива Столбец ну типа X и Y на координатах
+        {
+            matrix[i,j] = rnd.Next(min, max);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 4} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+   for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+
+    }
+    Console.Write("]");
+}
+
+double [] CreateArrayAvgForMatrixRow(int [,] matrix)
+{   
+    double [] array = new double[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            sum += matrix[i,j];
+        }
+    array[i] = (double) sum / matrix.GetLength(1);
+    }
+    return array;
+}
+
+int [,] array2D = CreateMatrixRndInt(4,3, 1, 10);
+PrintMatrix(array2D);
+
+Console.WriteLine("");
+
+double [] result = CreateArrayAvgForMatrixRow(array2D);
+PrintArray(result);
