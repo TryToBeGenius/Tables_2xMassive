7639f3abaec70cef6f7a5708f1be84fda2f98c1f
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-26 15:19:38 +0500

    Задача 2 Решена с двухзначными числами а не 3х

---
+++ LectionTask2/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = rnd.Next(10, 100);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void InterestedDigitOfMatrix(int[,] matrix)
+{   
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if ((matrix[i,j] / 10 + matrix[i,j] % 10) % 2 == 0)
+                {
+                    
+                    count ++;
+                    Console.Write($"{matrix[i, j]}, ");
+                }
+
+        }
+        
+    }
+    Console.Write($"Колличество чисел:{count}");
+}
+
+int [,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
+Console.Write("ИТОГО: ");
+InterestedDigitOfMatrix (matrix);
