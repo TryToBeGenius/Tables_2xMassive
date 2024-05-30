86d9b7b1b5e4d7684fd9bf9245d21c843084709b
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 18:32:26 +0500

    finish 1 zadachy

---
+++ Sem6Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿string CharArrayToString(char [] arr)
+{
+    string res ="";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res+= arr[i];
+    }
+    return res;
+
+}
+
+char [] array = { 'a', 'b', 'c', 'd', 'e', 'f'};
+string result = CharArrayToString(array);
+Console.WriteLine(result);
