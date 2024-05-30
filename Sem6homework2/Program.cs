6524ef21d74eee45bcf81d50fe64051014af1997
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 19:56:27 +0500

    Homework2 done

---
+++ Sem6homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿string Stroka1 = "AaBbCcDddDcCbBaA";
+
+string FromBigToSmallLeters(string s)
+{
+    string BigLetters = "ABCDEFGHIJKLMNOPQRSTWXYZ";
+    string SmallLetters = "abcdefghijqlmnopqrstwxyz";
+    string NewString = "";
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = 0; j < BigLetters.Length; j++)
+    {
+        if (s[i] == BigLetters[j])
+        {
+                
+            NewString += SmallLetters[j];
+        }
+        if (s[i] == SmallLetters[j])
+        {
+            NewString += SmallLetters[j];
+        }
+        
+    }
+    }
+    return NewString;
+}
+
+string result = FromBigToSmallLeters(Stroka1);
+Console.WriteLine(result);
