ddff64d060cf2f1361995204237e6e14a2ccc4ed
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 20:13:44 +0500

    homework3 done

---
+++ Sem6homework3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите строку:");
+string rndStr = Console.ReadLine();
+
+bool IsItPolindrom(string s)
+{
+    for(int i = 0; i < s.Length/2; i++)
+    {
+        if(s[i] == s[s.Length - i-1])
+        {
+            continue;
+        }
+        else 
+        {
+            Console.Write("Нет");
+            return false;
+            break;
+        }
+    }
+    Console.Write("Да");
+    return true;
+    
+}
+
+Console.Write("Это полиндром? :");
+IsItPolindrom(rndStr);
