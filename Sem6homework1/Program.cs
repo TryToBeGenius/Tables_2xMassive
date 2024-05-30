46b1d70a8f357c04cb45354d499b221d17d8f906
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 19:30:11 +0500

    Resheno DZ1

---
+++ Sem6homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿char [,] Table1 = {{'a', 'b', 'c', 'd'},{'r', 's', 'e', 'h'}};
+
+string FromMatrixToString(char [,] Matrix)
+{
+    string NewString = "";
+    for(int i =0; i < Matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < Matrix.GetLength(1); j++)
+        {
+            NewString += Matrix[i,j];
+        }
+    }
+    return NewString;
+}
+
+string result = FromMatrixToString(Table1);
+Console.WriteLine(result);
