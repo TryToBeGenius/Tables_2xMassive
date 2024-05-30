db50c9e9f72b53647aef0372f7f2e2c320fb6fe8
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 19:12:40 +0500

    Zadacha 3 reshena

---
+++ Sem6Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int SummOfVowels(string s)
+{
+    string glssnie = "aeiouy";
+    int cnt = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = 0; j < glssnie.Length; j++)
+        {
+            if (s[i] == glssnie[j])
+            {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+Console.WriteLine("Введите слово:");
+string Strnew = Console.ReadLine();
+Console.WriteLine($"В слове {SummOfVowels(Strnew)} гласных");
