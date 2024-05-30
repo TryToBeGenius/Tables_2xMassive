beadf6666b394eccab53d8387f1568bd99b82f83
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-30 18:52:03 +0500

    Zadacha2 Reshena

---
+++ Sem6Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+char[] FromStringToMassive(string s)
+{
+    
+    char[] array = new char[s.Length];
+    for (int i = 0; i < s.Length; i++)
+    {
+        array[i] = s[i];
+        
+    }
+return array;
+    
+}
+
+void PrintArray(char[] array1)
+{
+    Console.Write("[");
+   for (int i = 0; i < array1.Length; i++)
+    {
+        if (i < array1.Length - 1)
+        {
+            Console.Write($"{array1[i]}, ");
+        }
+        else
+        {
+            Console.Write(array1[i]);
+        }
+
+    }
+    Console.Write("]");
+}
+
+char [] resArray = FromStringToMassive(str);
+PrintArray(resArray);
